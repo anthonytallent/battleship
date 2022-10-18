13bdfefa0db127d158c962c56fea112c689736f6
Anthony Blackwell Tallent <anthonytallent567@gmail.com>
2022-10-18 16:59:12 -0700

    Make the attributes/respective tests and start the sunk? method

--- spec/ship_spec.rb
+++ spec/ship_spec.rb
@@ -1,9 +1,29 @@
 require './lib/ship'
 
 RSpec.describe Ship do
-  it 'exists' do
-    cruiser = Ship.new("Cruiser", 3)
+  describe '#Ship class' do
+    it 'exists' do
+      cruiser = Ship.new("Cruiser", 3)
 
-    expect(cruiser).to be_a(Ship)
+      expect(cruiser).to be_a(Ship)
+    end
+
+    it 'has a name' do
+      cruiser = Ship.new("Cruiser", 3)
+
+      expect(cruiser.name).to eq("Cruiser")
+    end
+
+    it 'has a length' do
+      cruiser = Ship.new("Cruiser", 3)
+
+      expect(cruiser.length).to eq(3)
+    end
+
+    it 'has a health attribute' do
+      cruiser = Ship.new("Cruiser", 3)
+
+      expect(cruiser.health).to eq(3)
+    end
   end
 end
