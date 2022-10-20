bdfc97c563e4771e3a3f4e9d5dd96e7ea49fca0b
Christian McCabe <christian.mccabe89@gmail.com>
2022-10-19 19:12:48 -0600

    Refactor the symbol names of the @cells hash

--- lib/board.rb
+++ lib/board.rb
@@ -6,22 +6,22 @@ class Board
 
   def initialize
     @cells = {
-      cell_1: Cell.new("A1"),
-      cell_2: Cell.new("A2"),
-      cell_3: Cell.new("A3"),
-      cell_4: Cell.new("A4"),
-      cell_5: Cell.new("B1"),
-      cell_6: Cell.new("B2"),
-      cell_7: Cell.new("B3"),
-      cell_8: Cell.new("B4"),
-      cell_9: Cell.new("C1"),
-      cell_10: Cell.new("C2"),
-      cell_11: Cell.new("C3"),
-      cell_12: Cell.new("C4"),
-      cell_13: Cell.new("D1"),
-      cell_14: Cell.new("D2"),
-      cell_15: Cell.new("D3"),
-      cell_16: Cell.new("D4")
+      A1: Cell.new("A1"),
+      A2: Cell.new("A2"),
+      A3: Cell.new("A3"),
+      A4: Cell.new("A4"),
+      B1: Cell.new("B1"),
+      B2: Cell.new("B2"),
+      B3: Cell.new("B3"),
+      B4: Cell.new("B4"),
+      C1: Cell.new("C1"),
+      C2: Cell.new("C2"),
+      C3: Cell.new("C3"),
+      C4: Cell.new("C4"),
+      D1: Cell.new("D1"),
+      D2: Cell.new("D2"),
+      D3: Cell.new("D3"),
+      D4: Cell.new("D4")
     }
   end
 end
