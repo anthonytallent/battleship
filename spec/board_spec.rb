e3d8f97495cadf93270ce34e6fa6523651c58548
Christian McCabe <christian.mccabe89@gmail.com>
2022-10-20 14:09:48 -0600

    Create test for overlapping ships

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -77,4 +77,18 @@ RSpec.describe Board do
       expect(board.valid_placement?(cruiser, ["B1", "C1", "D1"])).to eq(true)
     end
   end
+
+  describe 'overlapping ships' do
+    it 'tests if ships overlap (occupy the same cell)' do
+      board = Board.new
+
+      cruiser = Ship.new("Cruiser", 3)
+      submarine = Ship.new("Submarine", 2)
+
+      board.place(cruiser, ["A1", "A2", "A3"])
+
+      expect(board.valid_placement?(submarine, ["A1", "B1"])).to eq(false)
+    end 
+  end
+
 end
