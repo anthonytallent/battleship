6a25599f3bbfbff93fd0d92ee244e899c229b0da
Christian McCabe <christian.mccabe89@gmail.com>
2022-10-19 19:09:06 -0600

    Create valid_coordinate? tests

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -32,5 +32,23 @@ RSpec.describe Board do
     end
   end
 
+  describe '#valid_coordinate? method' do
+    it 'returns true if coordinate is valid' do
+      board = Board.new
+
+      expect(board.valid_coordinate?("A1")).to eq(true)
+      expect(board.valid_coordinate?("D4")).to eq(true)
+
+    end
+
+    it 'returns false if coordinate is invalid' do
+      board = Board.new
+
+      expect(board.valid_coordinate?("A5")).to eq(false)
+      expect(board.valid_coordinate?("E1")).to eq(false)
+      expect(board.valid_coordinate?("A22")).to eq(false)
+    end
+  end
+
 
 end
