729982082fe68bf8e3440cad9c7e10bf1db5533d
Anthony Blackwell Tallent <anthonytallent567@gmail.com>
2022-10-22 11:42:25 -0700

    Add the tests necessary for the comp placing ships

---
+++ spec/computer_spec.rb
@@ -0,0 +1,34 @@
+require 'pry'
+require './lib/ship'
+require './lib/cell'
+require './lib/board'
+require './lib/computer'
+require './lib/turn'
+
+RSpec.describe do
+  it 'exists' do
+    board = Board.new
+    computer = Computer.new
+    expect(computer).to be_a(Computer)
+  end
+
+  it 'will place its cruiser in valid coordinates' do
+    board = Board.new
+    computer = Computer.new
+    cruiser = Ship.new("Cruiser", 3)
+    
+    computer.place_cruiser(cruiser, computer.coordinates)
+
+    expect(board.valid_placement?(cruiser, computer.coordinates)).to eq(true)
+  end
+
+  it 'will place its submarine in valid coordinates' do
+    board = Board.new
+    computer = Computer.new
+    submarine = Ship.new("Submarine", 2)
+    
+    computer.place_submarine(submarine, computer.coordinates)
+
+    expect(board.valid_placement?(submarine, computer.coordinates)).to eq(true)
+  end
+end
