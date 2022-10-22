870b9bf540c973ef3347f4bf525f53670c3ba64f
Anthony Blackwell Tallent <anthonytallent567@gmail.com>
2022-10-22 11:42:44 -0700

    Make the place ship methods pass

---
+++ lib/computer.rb
@@ -0,0 +1,44 @@
+require 'pry'
+require './lib/ship'
+require './lib/cell'
+require './lib/board'
+require './lib/turn'
+
+class Computer
+  attr_reader :cruiser, :submarine, :board, :coordinates
+  attr_accessor :cruiser, :submarine, :board, :coordinates
+
+  def initialize
+    @cruiser = Ship.new("Cruiser", 3)
+    @submarine = Ship.new("Submarine", 2)
+    @board = Board.new
+    @coordinates = []
+  end
+
+  def place_cruiser(cruiser, valid_coordinates)
+      until board.valid_placement?(cruiser, valid_coordinates)
+          if board.valid_placement?(cruiser, valid_coordinates)
+            board.place(:cruiser, valid_coordinates)
+          else valid_coordinates = []
+          end
+        valid_coordinates = []
+        valid_coordinates << board.cells.keys.sample
+        valid_coordinates << board.cells.keys.sample
+        valid_coordinates << board.cells.keys.sample
+      end
+    @coordinates = valid_coordinates
+  end
+
+  def place_submarine(submarine, valid_coordinates)
+    until board.valid_placement?(submarine, valid_coordinates)
+        if board.valid_placement?(submarine, valid_coordinates)
+          board.place(:submarine, valid_coordinates)
+        else valid_coordinates = []
+        end
+      valid_coordinates = []
+      valid_coordinates << board.cells.keys.sample
+      valid_coordinates << board.cells.keys.sample
+    end
+  @coordinates = valid_coordinates
+end
+end
