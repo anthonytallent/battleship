cc7d17680be3a312ac0ce0193233ba3b98134bf5
Christian McCabe <christian.mccabe89@gmail.com>
2022-10-25 16:47:19 -0600

    Created/moved methods to Game class from Turn class

--- lib/game.rb
+++ lib/game.rb
@@ -7,7 +7,7 @@ require './lib/turn'
 require './lib/player'
 
 class Game
-  attr_reader :player, :computer, :computer_ships_sunk, :player_ships_sunk, :board, :cruiser, :submarine, :coordinates
+  attr_reader :player, :computer, :computer_ships_sunk, :player_ships_sunk, :board, :cruiser, :submarine, :coordinates, :x, :y
 
   def initialize
     @coordinates = []
@@ -18,7 +18,34 @@ class Game
     @computer = Computer.new
     @computer_ships_sunk = 0
     @player_ships_sunk = 0
+    @x
+    @y
 
+  end
+
+  def player_shot
+    puts "Enter coordinate for your shot:"
+    @x = gets.chomp.split[0]
+    computer.board.cells[@x].fire_upon
+  end
+
+  def computer_shot
+    @y = player.board.cells.keys.sample
+    until player.board.cells[@y].fired_upon? == false
+      @y = player.board.cells.keys.sample
+    end
+    player.board.cells[@y].fire_upon
+    binding.pry
+  end
+
+  def results
+    if computer.board.cells[@x].render == "X"
+      puts "Your shot on #{@x} sunk a ship."
+    elsif computer.board.cells[@x].render == "H"
+      puts "Your shot on #{@x} was a hit."
+    elsif computer.board.cells[@x].render == "M"
+      puts "Your shot on #{@x} was a miss."
+    end
   end
 
   def menu
@@ -42,6 +69,7 @@ class Game
     menu
     computer.place_cruiser(cruiser, coordinates)
     computer.place_submarine(submarine, coordinates)
+    binding.pry
     puts "I have laid out my ships on the grid." 
     puts "You now need to lay out your two ships."
     puts "The Cruiser is three units long and the Submarine is two units long."
@@ -56,10 +84,21 @@ class Game
 
   def play
   
-      turn = Turn.new(player, computer)
+      # turn = Turn.new(player, computer)
+      puts "=============COMPUTER BOARD============="
+      puts computer.board.render(optional_arg = false)
+      puts "==============PLAYER BOARD=============="
+      puts player.board.render(true)
       # binding.pry
-      computer.board.cells[gets.chomp].fire_upon
-    
+      # computer.board.cells[gets.chomp].fire_upon
+      player_shot
+      #binding.pry
+      computer_shot
+      #binding.pry
+      results
+      binding.pry
 
   end
+
+
 end
