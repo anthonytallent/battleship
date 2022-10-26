e774b2231417e841e075445e190bcf60b0828b5b
Christian McCabe <christian.mccabe89@gmail.com>
2022-10-25 19:42:52 -0600

    Finish Iteration 3

--- lib/player.rb
+++ lib/player.rb
@@ -8,6 +8,7 @@ require './lib/turn'
 class Player
   attr_reader :cruiser, :submarine, :board
   attr_accessor :cruiser, :submarine, :board
+  
   def initialize
     @cruiser = Ship.new("Cruiser", 3)
     @submarine = Ship.new("Submarine", 2)
@@ -22,7 +23,7 @@ class Player
         puts "or: A1 B1 C1"
         puts "no comma necessary"
         puts ""
-        cruiser_coord = gets.chomp.split
+        cruiser_coord = gets.chomp.upcase.split
           if board.valid_placement?(cruiser, cruiser_coord) == true
             @board.place(cruiser, cruiser_coord)
             coordinates = true
@@ -40,7 +41,7 @@ class Player
         puts "or: C1 D1"
         puts "no comma necessary"
         puts ""
-        submarine_coord = gets.chomp.split
+        submarine_coord = gets.chomp.upcase.split
           if board.valid_placement?(submarine, submarine_coord) == true
             @board.place(submarine, submarine_coord)
             coordinates = true
